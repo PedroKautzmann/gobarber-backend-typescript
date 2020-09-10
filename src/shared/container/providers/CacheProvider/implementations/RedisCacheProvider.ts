091a336b14df1cc7e6413ccac5f3729eb39a67ca
Pedro Kautzmann <pedrokautzmann1@gmail.com>
2020-09-09 23:17:37 -0300

    feature: tests refactored and rate limiter middleware

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -6,6 +6,7 @@ export default class RedisCacheProvider implements ICacheProvider {
   private client: RedisClient;
 
   constructor() {
+    console.log('teste');
     this.client = new Redis(cacheConfig.config.redis);
   }
 
